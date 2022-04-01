7a9447724671e2f23dd3095612c3cf952bfcc0f0
Edson Angoti Júnior <angoti@gmail.com>
2022-04-01 16:59:21 -0300

    Botão signin google

--- src/util/styles.js
+++ src/util/styles.js
@@ -1,6 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#18073f',
+  },
+  navbar: { backgroundColor: '#f4511e' },
   titulo: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -8,14 +14,26 @@ export const styles = StyleSheet.create({
     padding: 8,
   },
   button: {
-    alignItems: 'center',
-    backgroundColor: '#DDD',
-    borderColor: '#aaa',
-    borderWidth: 1,
-    padding: 2,
+    width: 192,
+    height: 48,
+    alignSelf: 'center',
+  },
+  logoutButton: {
+    // alignItems: 'center',
+    // backgroundColor: '#DDD',
+    // borderColor: '#aaa',
+    // borderWidth: 1,
+    // padding: 4,
+    color: '#fff',
   },
   mensagens: {
     flex: 1,
   },
   card: { borderColor: '#f00', borderWidth: 2 },
+  separador: {
+    height: 1,
+    width: '100%',
+    backgroundColor: '#aaa',
+    marginBottom: 4,
+  },
 });
